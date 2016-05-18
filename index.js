9ce85765e4f4e518408281cf94ca20489c27555e
Mitch Kroska <kroskam8457@hotmail.com>
2016-05-17 23:14:51 -0500

    Try to modularize project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
-var meanSeed = require('./modules/meanSeed.js');
+// var meanSeed = require('./modules/meanSeed.js');
 
-var seedData = meanSeed.schemaToSeedData(10, {"f_name":"f_name"});
+// var seedData = meanSeed.schemaToSeedData(10, {"f_name":"f_name"});
 
-var seedDataObj
+// var seedDataObj
